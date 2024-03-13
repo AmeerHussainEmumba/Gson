a6bde1e872020c9314abc2c239dc5eb360ac36f4
AmeerHussainEmumba <ameer.hussain@emumba.com>
2024-03-13 22:41:08 +0500

    BookProcessor now returns collection of books which match the query

--- src/main/java/LibraryActions/BookProcessor.java
+++ src/main/java/LibraryActions/BookProcessor.java
@@ -1,63 +1,57 @@
 package LibraryActions;
-import LibraryActions.Library;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class BookProcessor {
 
 
-    public void displayBookInfo(Library.BookObject[] books) {
-        for (Library.BookObject book : books)
-        {
-            log.info("Title: " + book.getTitle());
-            log.info("Author: " + book.getAuthor());
-            log.info("Price: " + book.getMetadata().getPrice());
-            log.info("Categories: ");
-            for (String category : book.getMetadata().getCategories())
-                log.info("  - " + category);
-            log.info("ISBN: " + book.getMetadata().getIsbn());
-            log.info("Pages: " +book.getMetadata().getPages());
-        }
+    public List<Library.BookObject> displayBookInfo(Library.BookObject[] books) {
+
+        return Arrays.stream(books).toList();
     }
 
-    public void allBooksOfAnAuthor (Library.BookObject[] books, String author)
-    {
-        Arrays.stream(books)
+    public List<String> allBooksOfAnAuthor (Library.BookObject[] books, String author)
+    {   String pretext= "these are the book(s) of the author "+author;
+         List<String>returnBooksOfAuthor= Arrays.stream(books)
                 .filter(book -> Objects.equals(book.getAuthor(), author))
                 .map(Library.BookObject::getTitle)
-                .forEach(log::info);
+                 .collect(Collectors.toList());
+          returnBooksOfAuthor.addFirst(pretext);
+          return returnBooksOfAuthor;
     }
 
-    public void getAveragePrice(Library.BookObject[] books)
+    public double getAveragePrice(Library.BookObject[] books)
     {
-        double averagePrice = Arrays.stream(books)
+       return Arrays.stream(books)
                 .mapToDouble(book -> book.getMetadata().getPrice())
                 .average()
                 .orElse(0);
-        log.info("The Average price is " + averagePrice);
+
     }
 
-    public void getHighestPrice (Library.BookObject[] books)
+    public List<Library.BookObject> getHighestPrice (Library.BookObject[] books)
     {
-        int highestPrice = Arrays.stream(books)
+         int highestPrice = Arrays.stream(books)
                 .mapToInt(book -> book.getMetadata().getPrice())
                 .max()
                 .orElse(0);
-        Arrays.stream(books)
-                .filter(book -> book.getMetadata().getPrice() == highestPrice)
-                .forEach(book -> log.info(book.getTitle() + " has the highest price, which is " + book.getMetadata().getPrice()));
+            return Arrays.stream(books)
+                .filter(book -> book.getMetadata().getPrice() == highestPrice).toList();
     }
 
-    public void getBookOfCategory (Library.BookObject[] books, String category)
-    {   Arrays.stream(books)
+    public List<String> getBookOfCategory (Library.BookObject[] books, String category)
+    {    String pretext="these are the book(s) of the category "+category;
+        List<String> returnBooksOfCategory= Arrays.stream(books)
             .filter(book -> Arrays.asList(book.getMetadata().getCategories()).contains(category))
-            .map(Library.BookObject::getTitle)
-            .forEach(log::info);
+            .map(Library.BookObject::getTitle).collect(Collectors.toList());
+         returnBooksOfCategory.addFirst(pretext);
+         return returnBooksOfCategory;
     }
-
-
 }
 
--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,25 +1,44 @@
 package org.example;
 
 import LibraryActions.Library;
+import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
+
+@Slf4j
 public class MainApp {
     public static void main(String[] args) {
         Library library= new Library();
         Library.BookObject[] booksInLibrary= library.addBooksToLibrary("src/main/resources/books.json");
         {
             //Display AllBooks
-            library.getBookProcessor().displayBookInfo(booksInLibrary);
+           List<Library.BookObject> allBookData=library.getBookProcessor().displayBookInfo(booksInLibrary);
+            allBookData.forEach(book -> {
+                log.info("Title: " + book.getTitle());
+                log.info("Author: " + book.getAuthor());
+                log.info("Price: " + book.getMetadata().getPrice());
+                log.info("Categories: ");
+                for (String category : book.getMetadata().getCategories())
+                    log.info("  - " + category);
+                log.info("ISBN: " + book.getMetadata().getIsbn());
+                log.info("Pages: " +book.getMetadata().getPages());
+                log.info("--------");
+            });
 
-            //Get All books of an Author xyz
-            library.getBookProcessor().allBooksOfAnAuthor(booksInLibrary, "An Author");
-//
-//          //Get Average price
-            library.getBookProcessor().getAveragePrice(booksInLibrary);
-//
-//          //Get book with the highest price
-            library.getBookProcessor().getHighestPrice(booksInLibrary);
-//
-//          //Get books with certain category
-            library.getBookProcessor().getBookOfCategory(booksInLibrary, "Thriller");
+         //Get All books of an Author xyz
+             List<String> allBooksOfAnAuthor=library.getBookProcessor().allBooksOfAnAuthor(booksInLibrary, "An Author");
+             allBooksOfAnAuthor.forEach(log::info);
+
+         //Get Average price
+            log.info("The Average price is " +library.getBookProcessor().getAveragePrice(booksInLibrary));
+
+         //Get book with the highest price
+            List<Library.BookObject> highestPrice =library.getBookProcessor().getHighestPrice(booksInLibrary);
+            highestPrice.forEach(book -> log.info(book.getTitle() + " has the highest price, which is " + book.getMetadata().getPrice()));
+
+       //Get books with certain category
+            List<String> booksOfCategory= library.getBookProcessor().getBookOfCategory(booksInLibrary, "Thriller");
+            booksOfCategory.forEach(log::info);
         }
 }
 }
